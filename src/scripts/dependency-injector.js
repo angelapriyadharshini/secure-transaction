7bbd5afd4356cb1227455f9e9f642f185157211c
angelapriyadharshini <angela.amarapala@gmail.com>
2020-07-12 04:01:16 +0530

    Include controller and service in DI

--- src/scripts/dependency-injector.js
+++ src/scripts/dependency-injector.js
@@ -8,22 +8,32 @@ module.exports = function loadModules(awilix) {
     awilix: awilix.asValue(require('awilix')),
     express: awilix.asValue(require('express')),
     app: awilix.asValue(require('express')()),
-    bodyParser: awilix.asValue(require('body-parser'))
+    bodyParser: awilix.asValue(require('body-parser')),
+    cors: awilix.asValue(require('cors')),
+    fs: awilix.asValue(require("fs"))
   });
 
   container.loadModules([
     ['src/config/config.js', { register: awilix.asFunction }],
     ['src/transactions/router.js', { register: awilix.asFunction }],
     ['src/transactions/controllers/*.js', { register: awilix.asClass }],
-    ['src/transactions/business-logics/*.js', { register: awilix.asClass }],
+    ['src/transactions/services/*.js', { register: awilix.asClass }],
+    ['src/transactions/middlewares/*.js', { register: awilix.asClass }],
+    ['src/transactions/utils/*.js', { register: awilix.asFunction }]
   ], {
     formatName: 'camelCase',
     resolverOptions: {
-      injectionMode: InjectionMode.PROXY,
       lifetime: awilix.Lifetime.SINGLETON
     }
   });
 
-  return container;
+  return {
+    container,
+    initiateModule: function (module, moduleName) {
+      var diObj = {}
+      diObj[moduleName] = awilix.asValue(module);
+      container.register(diObj);
+    }
+  };
 
 }(require('awilix'))
